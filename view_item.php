856ad83efd26d7370dff2c3df5e1de6ab2ee33b2
Max Kremmel <xing@synapse.plus.com>
2006-09-11 14:53:29 +0000

    load random gallery when no gallery is specified

--- view_item.php
+++ view_item.php
@@ -8,18 +8,22 @@ require_once( TREASURY_PKG_PATH.'TreasuryItem.php');
 $extras = TRUE;
 require_once( TREASURY_PKG_PATH.'item_lookup_inc.php');
 
+// load the parent gallery as well
 if( @BitBase::verifyId( $_REQUEST['structure_id'] ) ) {
-	$galleryDisplayPath = $gContent->getDisplayPath( $gContent->getGalleryPath( $_REQUEST['structure_id'] ) );
-	$gBitSmarty->assign( 'galleryDisplayPath', $galleryDisplayPath );
+	$gGallery = new TreasuryGallery( $_REQUEST['structure_id'] );
+	$gGallery->load();
 } else {
 	// if we don't have a structure id to go by, we just get one
 	$galleryContentIds = $gContent->getGalleriesFromItemContentId( $gContent->mContentId );
 	if( @BitBase::verifyId( $galleryContentIds[0] ) ) {
-		$gallery = new TreasuryGallery( $galleryContentIds[0] );
-		$galleryDisplayPath = $gContent->getDisplayPath( $gContent->getGalleryPath( $gallery->mStructureId ) );
-		$gBitSmarty->assign( 'galleryDisplayPath', $galleryDisplayPath );
+		$gGallery = new TreasuryGallery( NULL, $galleryContentIds[0] );
+		$gGallery->load();
 	}
 }
+
+$galleryDisplayPath = $gContent->getDisplayPath( $gContent->getGalleryPath( $gGallery->mStructureId ) );
+$gBitSmarty->assign( 'galleryDisplayPath', $galleryDisplayPath );
+$gBitSmarty->assign_by_ref( 'gGallery', $gGallery );
 
 $gBitSystem->display( "bitpackage:treasury/view_item.tpl", tra( "View File" ) );
 ?>
