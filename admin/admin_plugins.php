f9153451051dd66b68dda09374de2b51d296818a
Max Kremmel <xing@synapse.plus.com>
2007-01-20 10:24:12 +0000

    add button to reset plugin settings

--- admin/admin_plugins.php
+++ admin/admin_plugins.php
@@ -1,8 +1,15 @@
 <?php
 require_once( '../../bit_setup_inc.php' );
+$gBitSystem->verifyPermission( 'p_admin' );
+
 include_once( KERNEL_PKG_PATH.'simple_form_functions_lib.php' );
 
-$gBitSystem->verifyPermission( 'p_admin' );
+// this will clear out all plugin settings in the database.
+if( !empty( $_REQUEST['reset_all_plugins'] ) ) {
+	$gTreasurySystem->resetAllPluginSettings();
+	// reload page that everything is displayed as it actually is
+	header( "Location: ".TREASURY_PKG_URL."admin/admin_plugins.php" );
+}
 
 $gTreasurySystem->scanAllPlugins( TREASURY_PKG_PATH.'plugins/' );
 
@@ -12,8 +19,6 @@ if( !empty( $_REQUEST['pluginsave'] ) ) {
 	$feedback['success'] = tra( 'The plugins were successfully updated' );
 }
 $gBitSmarty->assign( 'feedback', $feedback );
-
-//vd($gTreasurySystem->mPlugins);
 
 $gBitSystem->display( 'bitpackage:treasury/admin_plugins.tpl', tra( 'Plugins' ) );
 ?>
