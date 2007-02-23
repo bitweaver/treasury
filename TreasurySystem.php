f7177e491e95a68e19b2a40c75a74a107a15b4dc
Max Kremmel <xing@synapse.plus.com>
2007-02-23 12:21:04 +0000

    fix LibertySystem to be extendable again, update nexus and treasury to work with new LibertySystem

--- TreasurySystem.php
+++ TreasurySystem.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @version      $Header: /cvsroot/bitweaver/_bit_treasury/Attic/TreasurySystem.php,v 1.8 2006/10/13 12:47:20 lsces Exp $
+ * @version      $Header: /cvsroot/bitweaver/_bit_treasury/Attic/TreasurySystem.php,v 1.9 2007/02/23 12:21:04 squareing Exp $
  *
  * @author       xing  <xing@synapse.plus.com>
- * @version      $Revision: 1.8 $
+ * @version      $Revision: 1.9 $
  * created      Monday Jul 03, 2006   11:06:47 CEST
  * @package      treasury
  * @copyright    2003-2006 bitweaver
@@ -34,8 +34,9 @@ class TreasurySystem extends LibertySystem {
 	 * @return void
 	 */
 	function TreasurySystem() {
-		// Set the package using LibertySystem
-		$this->mSystem = TREASURY_PKG_NAME;
+		// Set some treasury specific options that we can extend LibertySystem
+		$this->mSystem     = TREASURY_PKG_NAME;
+		$this->mPluginPath = TREASURY_PKG_PATH."plugins/";
 
 		LibertySystem::LibertySystem( FALSE );
 	}
