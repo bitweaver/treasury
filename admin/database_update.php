36a2bf9a7d4378ce86f24b47f210b57c9aa8a396
Max Kremmel <xing@synapse.plus.com>
2007-06-16 00:19:05 +0000

    fix some indexes and query. sorry about the sloppy code.

--- admin/database_update.php
+++ admin/database_update.php
@@ -11,12 +11,12 @@ $query = "
 		INNER JOIN `".BIT_DB_PREFIX."liberty_files` lf ON( lf.`file_id` = la.`foreign_id` )
 		INNER JOIN `".BIT_DB_PREFIX."liberty_attachments_map` lam ON( la.`attachment_id` = lam.`attachment_id` )
 		INNER JOIN `".BIT_DB_PREFIX."treasury_item` tri ON( tri.`content_id` = lam.`content_id` )
-	WHERE la.`attachment_plugin_guid` = ?";
+	WHERE la.`attachment_plugin_guid` = ? ORDER BY la.`attachment_id`";
 if( $ret = $gBitSystem->mDb->getAll( $query, array( PLUGIN_GUID_TREASURY_FILE ))) {
 	foreach( $ret as $update ) {
 		if( LibertyAttachable::storePrimaryAttachmentId( $update )) {
-			echo ">>>> Updated Treasury file: {$update['storage_path']}\n";
-			$gBitSystem->mDb->query( "DELETE FROM `".BIT_DB_PREFIX."liberty_attachments_map` WHERE `content_id`=? AND `attachment_id`", array( $update['content_id'], $update['attachment_id'] ));
+			echo ">>>> Updated Treasury file: [{$update['primary_attachment_id']}] {$update['storage_path']}\n";
+			$gBitSystem->mDb->query( "DELETE FROM `".BIT_DB_PREFIX."liberty_attachments_map` WHERE `content_id`=? AND `attachment_id` = ?", array( $update['content_id'], $update['primary_attachment_id'] ));
 		}
 	}
 }
