57bb9196923be7ac09d8aace85c7f282137e5586
Max Kremmel <xing@synapse.plus.com>
2008-06-01 09:06:30 +0000

    strip unused code

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author   xing <xing@synapse.plus.com>
- * @version  $Revision: 1.17 $
+ * @version  $Revision: 1.18 $
  * @package  Treasury
  * @subpackage functions
  */
@@ -17,21 +17,6 @@ $gBitSystem->registerPackage( $registerHash );
 define( 'TREASURY_CONTENT_TYPE_GUID', 'treasury' );
 
 if( $gBitSystem->isPackageActive( 'treasury' ) ) {
-	/*
-	// set up treasury system
-	require_once( TREASURY_PKG_PATH.'TreasurySystem.php' );
-	global $gTreasurySystem;
-	$gTreasurySystem = new TreasurySystem();
-	$plugin_status = $gBitSystem->getConfig( TREASURY_PKG_NAME.'_plugin_status_'.TREASURY_DEFAULT_MIME_HANDLER );
-	if( empty( $plugin_status ) || $plugin_status != 'y' ) {
-		$gTreasurySystem->scanAllPlugins( NULL, "mime\." );
-	} else {
-		$gTreasurySystem->loadActivePlugins();
-	}
-
-	$gBitSmarty->assign_by_ref( 'gTreasurySystem', $gTreasurySystem );
-	 */
-
 	if( $gBitUser->hasPermission( 'p_treasury_view_gallery' ) ) {
 		$menuHash = array(
 			'package_name'       => TREASURY_PKG_NAME,
@@ -40,11 +25,6 @@ if( $gBitSystem->isPackageActive( 'treasury' ) ) {
 			'admin_comments_url' => TREASURY_PKG_URL.'admin/admin_plugins.php',
 		);
 		$gBitSystem->registerAppMenu( $menuHash );
-	}
-
-	// if the flashvideo plugin is active, we need to load the js file since we don't know where the plugins is being used
-	if( $gLibertySystem->isPluginActive( 'dataflashvideo' )) {
-		$gBitThemes->loadJavascript( TREASURY_PKG_PATH."libs/flv_player/swfobject.js", FALSE, 25 );
 	}
 }
 ?>
