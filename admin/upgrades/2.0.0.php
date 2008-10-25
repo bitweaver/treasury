8152afc5cd698fa448dfafec0233cfb44f9e4c63
Max Kremmel <xing@synapse.plus.com>
2008-10-25 08:30:51 +0000

    more work towards package upgrades. things seem to working quite smoothly now.

---
+++ admin/upgrades/2.0.0.php
@@ -0,0 +1,12 @@
+<?php
+global $gBitInstaller;
+
+$infoHash = array(
+	'package'      => TREASURY_PKG_NAME,
+	'version'      => str_replace( '.php', '', basename( __FILE__ )),
+	'description'  => "Since you've been using treasury, please visit the <a href='".TREASURY_PKG_URL."admin/database_to_libertymime.php'>treasury upgrade script</a> after you've completed the upgrade.",
+	'post_upgrade' => "Since you've been using treasury, please visit the <a href='".TREASURY_PKG_URL."admin/database_to_libertymime.php'>treasury upgrade script</a> after you've completed the upgrade.",
+);
+
+$gBitInstaller->registerPackageUpgrade( $infoHash );
+?>
